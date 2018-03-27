19de7a57afda54139e59483a307d6bde0a402103
Guilherme Maegava <p000gmaegava@prservicos.com.br>
2018-03-26 22:01:50 -0300

    digitalsign first commit

---
+++ src/main/java/br/com/maegava/digitalsign/DigitalSignApp.java
@@ -0,0 +1,11 @@
+package br.com.maegava.digitalsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignApp {
+	public static void main (String[] args) {
+		SpringApplication.run(DigitalSignApp.class, args);
+	}
+}
---
+++ src/main/java/br/com/maegava/digitalsign/service/DocusignService.java
@@ -0,0 +1,11 @@
+package br.com.maegava.digitalsign.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocusignService {
+	
+	public void sendDocument() {
+		
+	}
+}
